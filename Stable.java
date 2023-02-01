8112f471d99a108345e5cfb80ac547e6ff78fdc3
trevorSatoriSFA <112034859+trevorSatoriSFA@users.noreply.github.com>
2023-02-01 13:21:35 -0600

    Update Stable.java

--- Stable.java
+++ Stable.java
@@ -2,5 +2,9 @@ public class Stable{
   public static void main(String[] args){
     System.out.println("SemiColons Are Important.")
   }
+  
+  public static void uselessFunction(){
+    System.out.println("WHAT A TIME TO BE ALIVE");
+  }
 
 }
